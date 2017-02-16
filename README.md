ec821ffb7a517370b4cc6e430cd45e18f7f2ef7a
Yasmany Santalla Pereda <ysantalla@upr.edu.cu>
2017-02-16 10:36:56 -0500

    actualizando app

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# Proyect version 0.1.0
-
-# Instalar paquetes python
-apt-get install python-pymongo o pip install pymongo
+# proyect
--- controllers/especialista.py
+++ controllers/especialista.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
-def __validate(form, id_diagnostico, id_pregunta):
+def get_pregunta():
     '''
-    Validando que exista un solo tipo de pregunta por cuestionario
+    Componente que devuelve las preguntas de un tipo dado para el cuestionario activo
     '''
-    if id_pregunta:
-        pregunta = db((db.pregunta.diagnostico == id_diagnostico) & (db.pregunta.tipo == form.vars.tipo)).select().first()
-        if pregunta:
-            if str(pregunta.id) != id_pregunta:
-                form.errors.tipo = T("Ya existe una pregunta de tipo para este cuestionario")
+    if request.cid:
+        preguntas = db((db.pregunta.diagnostico == request.args(0)) & (db.pregunta.tipo == request.args(1))).select(orderby=db.pregunta.ensayo|db.pregunta.created_on)
+        return dict(preguntas=preguntas)
     else:
-        if not (db((db.pregunta.diagnostico == id_diagnostico) & (db.pregunta.tipo == form.vars.tipo)).isempty()):
-            form.errors.tipo = T("Ya existe una pregunta de tipo para este cuestionario")
+        raise HTTP(403)
 
 def __total_puntos(id):
     '''
@@ -27,7 +24,7 @@ def gestionar_diagnostico():
     CRUD completo para gestionar diagnosticos kike tiene que ser asi
     '''
 
-    diagnostico = preguntas = onvalidation = selectable = orderby = fields = None
+    diagnostico = preguntas = selectable = orderby = total_puntos = fields = None
     links = []
     linked_tables=[]
 
@@ -36,26 +33,23 @@ def gestionar_diagnostico():
         response.flash = T('Gestionar preguntas del diagnóstico')
         mensaje = T('Gestionar preguntas del diagnóstico con identificador: ') + request.args(-1)
         diagnostico = db.diagnostico(request.args(2))
-        preguntas = diagnostico.pregunta.select(orderby=db.pregunta.tipo)
+        total_puntos = __total_puntos(diagnostico.id)
+        preguntas = diagnostico.pregunta.select(orderby=db.pregunta.tipo|db.pregunta.ensayo)
         selectable = lambda ids: db(db.pregunta.id.belongs(ids)).delete()
-        orderby = [db.pregunta.tipo]
-        fields = [db.pregunta.id, db.pregunta.descripcion, db.pregunta.tipo, db.pregunta.puntos, db.pregunta.tiempo]
-        onvalidation = lambda form : __validate(form, request.args(2), None)
+        orderby = [db.pregunta.tipo|db.pregunta.ensayo|db.pregunta.created_on]
+        fields = [db.pregunta.texto, db.pregunta.tipo, db.pregunta.ensayo, db.pregunta.puntos, db.pregunta.tiempo, db.pregunta.intentos]
     elif (("pregunta.diagnostico" in request.args) and ("pregunta" in request.args) and ("new" in request.args)):
         response.title = T('Pregunta del diagnóstico') + response.title
         response.flash = T('Pregunta del diagnóstico')
         mensaje = T('Pregunta del diagnóstico')
-        onvalidation = lambda form : __validate(form, request.args(2), None)
     elif (("pregunta.diagnostico" in request.args) and ("pregunta" in request.args) and ("view" in request.args)):
         response.title = T('Pregunta del diagnóstico') + response.title
         response.flash = T('Pregunta del diagnóstico')
         mensaje = T('Pregunta del diagnóstico')
-        onvalidation = lambda form : __validate(form, request.args(2), None)
     elif (("pregunta.diagnostico" in request.args) and ("pregunta" in request.args) and ("edit" in request.args)):
         response.title = T('Pregunta del diagnóstico') + response.title
         response.flash = T('Pregunta del diagnóstico')
         mensaje = T('Pregunta del diagnóstico')
-        onvalidation = lambda form : __validate(form, request.args(2), request.args(-1))
     else:
         response.title = T('Gestionar Diagnósticos') + response.title
         response.flash = T('Gestionar Diagnósticos')
@@ -66,20 +60,20 @@ def gestionar_diagnostico():
 
         links.append({'header': T('Total de puntos'), 'body': lambda row: __total_puntos(row.id)})
 
-    grid = SQLFORM.smartgrid(db.diagnostico, selectable=selectable, linked_tables=linked_tables, links=links, fields=fields, orderby=orderby, onvalidation=onvalidation)
+    grid = SQLFORM.smartgrid(db.diagnostico, selectable=selectable, linked_tables=linked_tables, links=links, fields=fields, orderby=orderby)
 
     heading=grid.elements('th')
     if heading:
            heading[0].append(INPUT(_type='checkbox', _onclick="jQuery('input[type=checkbox]').each(function(k){jQuery(this).attr('checked', 'checked');});"))
 
-    return dict(grid=grid, mensaje=mensaje, diagnostico=diagnostico, preguntas=preguntas)
+    return dict(grid=grid, mensaje=mensaje, diagnostico=diagnostico, preguntas=preguntas, total_puntos=total_puntos)
 
 @auth.requires_membership("Especialista")
 def nuevo_diagnostico_inicial():
     response.flash = T("Nuevo Diagnóstico Inicial")
     response.title = T("Nuevo Diagnóstico Inicial") + response.title
 
-    form = SQLFORM(db.diagnostico, fields=['titulo', 'descripcion', 'activo'])
+    form = SQLFORM(db.diagnostico, fields=['titulo', 'descripcion' 'activo', 'tiempo'])
 
     if form.validate():
         diagnostico = dict(form.vars)
